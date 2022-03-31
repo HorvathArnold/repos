d53fdf51d5be94f7c428e72ddad5350717c8a8da
HorvathArnold <94853221+HorvathArnold@users.noreply.github.com>
2022-03-31 17:30:37 +0200

    Add files via upload

---
+++ kor2/kor2/Hasab.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kor2
+{
+    internal class Hasab : Kor2
+    {
+        //osztalyvaltozó
+        private double magas,
+            terfogat,
+            felszin;
+
+        public Hasab() : base()
+        {
+        }
+        public void setMagas(double p) { this.magas = p; }
+        public double getMagas() { return this.magas; }
+        public void setTerfogat() { this.terfogat = Math.Round(this.getTerulet() * this.magas, 2); }
+        public double getTerfogat() { return this.terfogat; }
+        public void setFelszin()
+        {
+            this.felszin = Math.Round(this.getTerulet() * 2 + this.magas *
+this.getKerulet(), 2);
+        }
+        public double getFelszin() { return this.felszin; }
+
+    }
+}
